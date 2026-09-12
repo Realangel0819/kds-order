b754898f9ba52845155aaac7ea844835a6a6c910
Realangel0819 <dlrlwk010@naver.com>
2026-09-12 16:23:49 +0900

    feat: Spring Boot 프로젝트 세팅 및 Hello API 구현

---
+++ src/main/java/pengyu/kds_order/KdsOrderApplication.java
@@ -0,0 +1,13 @@
+package pengyu.kds_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdsOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdsOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pengyu/kds_order/KdsOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package pengyu.kds_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdsOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
